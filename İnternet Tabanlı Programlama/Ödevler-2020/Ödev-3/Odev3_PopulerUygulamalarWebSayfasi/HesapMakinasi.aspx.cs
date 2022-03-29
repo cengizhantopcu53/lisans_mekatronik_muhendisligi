a0fb660966eb1facb44f71fcb5da41c9ff125733
Cengizhan <64609951+cengizhantopcu53@users.noreply.github.com>
2022-03-29 16:27:22 +0300

    Add files via upload

---
+++ İnternet Tabanlı Programlama/Ödevler-2020/Ödev-3/Odev3_PopulerUygulamalarWebSayfasi/HesapMakinasi.aspx.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Odev3_PopulerUygulamalarWebSayfasi
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TxtEkran.Text = "0";
+        }
+
+        int islem = 0;
+        double sayi1 = 0, sayi2 = 0;
+
+        protected void Button10_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+            {
+                TxtEkran.Text = "";
+            }
+            TxtEkran.Text += "1";
+        }
+
+        protected void Button13_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+            {
+                TxtEkran.Text = "";
+            }
+            TxtEkran.Text += "2";
+        }
+
+        protected void Button16_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "3";
+        }
+
+        protected void Button11_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "4";
+        }
+
+        protected void Button14_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "5";
+        }
+
+        protected void Button17_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "6";
+        }
+
+        protected void Button12_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "7";
+        }
+
+        protected void Button15_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "8";
+        }
+
+        protected void Button18_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "9";
+        }
+
+        protected void Button25_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += "0";
+        }
+
+        protected void Button26_Click(object sender, EventArgs e)
+        {
+            if (TxtEkran.Text == "0")
+                TxtEkran.Text = "";
+            TxtEkran.Text += ",";
+        }
+
+        protected void BtnSilme_Click(object sender, EventArgs e)
+        {
+            sifirla();
+        }
+
+        public void sifirla()
+        {
+            TxtEkran.Text = "0";
+        }
+
+        protected void BtnCikarma_Click(object sender, EventArgs e)
+        {
+            islem = 2;
+            sayi1 = double.Parse(TxtEkran.Text);
+            sifirla();
+        }
+
+        protected void BtnCarpma_Click(object sender, EventArgs e)
+        {
+            islem = 3;
+            sayi1 = double.Parse(TxtEkran.Text);
+            sifirla();
+        }
+
+        protected void BtnBolme_Click(object sender, EventArgs e)
+        {
+            islem = 4;
+            sayi1 = double.Parse(TxtEkran.Text);
+            sifirla();
+        }
+
+        protected void BtnEsittir_Click(object sender, EventArgs e)
+        {
+            sayi2 = double.Parse(TxtEkran.Text);
+            TxtEkran.Text = hesapla().ToString("#,#.00");
+        }
+
+        public double hesapla()
+        {
+            double sonuc = 0;
+            if (islem == 1)
+                sonuc = sayi1 + sayi2;
+            else if (islem == 2)
+                sonuc = sayi1 - sayi2;
+            else if (islem == 3)
+                sonuc = sayi1 * sayi2;
+            else if (islem == 4)
+                sonuc = sayi1 / sayi2;
+            else
+                sonuc = 0;
+            return sonuc;
+        }
+
+        protected void BtnHaneSilme_Click(object sender, EventArgs e)
+        {
+            string veri = TxtEkran.Text;
+            TxtEkran.Text = "";
+            int i;
+            for (i = 0; i < veri.Length - 1; i++)
+            {
+                TxtEkran.Text += veri[i].ToString();
+            }
+        }
+
+        protected void BtnTopla_Click(object sender, EventArgs e)
+        {
+            islem = 1;
+            sayi1 = double.Parse(TxtEkran.Text);
+            sifirla();
+        }
+    }
+}
---
+++ İnternet Tabanlı Programlama/Ödevler-2020/Ödev-3/Odev3_PopulerUygulamalarWebSayfasi/IdealKiloHesaplama.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Sablon.Master" AutoEventWireup="true" CodeBehind="IdealKiloHesaplama.aspx.cs" Inherits="Odev3_PopulerUygulamalarWebSayfasi.IdealKiloHesaplama" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Image ID="Image2" runat="server" ImageUrl="~/Resimler/İDEAL KİLO HESAPLAMA.png" />
+    <br />
+    &nbsp;<br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Label ID="Label1" runat="server" Text="Kilonuzu Girin:"></asp:Label>
+&nbsp; <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <br />
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Label ID="Label3" runat="server" Text="Boyunuzu Girin:"></asp:Label>
+&nbsp;<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <asp:Button ID="Button1" runat="server" CssClass="auto-style18" Height="38px" Text="GÖSTER" Width="130px" OnClick="Button1_Click" style="margin-left: 153px" />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Button ID="Button2" runat="server" BackColor="#33CC33" Height="38px" OnClick="Button2_Click" Text="Temizle" Width="91px" />
+    <br />
+    <br />
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+     <asp:Label ID="Label2" runat="server" Text="SONUCUNUZ HESAPLANIYOR ..."></asp:Label>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:Image ID="Image3" runat="server" Height="80px" ImageUrl="~/Resimler/İDEAL KİLO HESAPLAMA-GÖRSEL1.jpg" Visible="False" Width="100px" />
+&nbsp;
+    <asp:Image ID="Image4" runat="server" Height="80px" ImageUrl="~/Resimler/İDEAL KİLO HESAPLAMA-GÖRSEL2.jpg" Visible="False" Width="100px" />
+&nbsp;
+    <asp:Image ID="Image5" runat="server" Height="80px" ImageUrl="~/Resimler/İDEAL KİLO HESAPLAMA-GÖRSEL3.jpg" Visible="False" Width="100px" />
+    <br />
+&nbsp; 
+</asp:Content>
