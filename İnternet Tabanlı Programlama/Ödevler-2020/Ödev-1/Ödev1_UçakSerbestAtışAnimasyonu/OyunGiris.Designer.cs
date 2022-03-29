0e53a276fa7c202ff33a2ace0a19c2526878a314
Cengizhan <64609951+cengizhantopcu53@users.noreply.github.com>
2022-03-29 16:24:03 +0300

    Add files via upload

---
+++ İnternet Tabanlı Programlama/Ödevler-2020/Ödev-1/Ödev1_UçakSerbestAtışAnimasyonu/OyunGiris.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Ödev1_UçakSerbestAtışAnimasyonu
+{
+    partial class OyunGiris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OyunGiris));
+            this.label7 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.DarkRed;
+            this.label7.Location = new System.Drawing.Point(208, 9);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(734, 78);
+            this.label7.TabIndex = 12;
+            this.label7.Text = resources.GetString("label7.Text");
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(479, 221);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 47);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "DOĞRULA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(315, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "KULLANICI ADI";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(385, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ŞİFRE";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(133, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(142, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 28);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ŞİFRE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "KULLANICI ADI";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 10);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 21);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "KAYIT";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(315, 7);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(52, 21);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "GİRİŞ";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Corbel", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(446, 23);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 28);
+            this.textBox3.TabIndex = 16;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(446, 60);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(133, 22);
+            this.textBox4.TabIndex = 17;
+            this.textBox4.UseSystemPasswordChar = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkRed;
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Location = new System.Drawing.Point(248, 108);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(613, 107);
+            this.panel1.TabIndex = 18;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Showcard Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.DarkRed;
+            this.label5.Location = new System.Drawing.Point(33, 529);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(154, 26);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "BEKLEYİNİZ...";
+            // 
+            // OyunGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1114, 591);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "OyunGiris";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OyunGiris";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ İnternet Tabanlı Programlama/Ödevler-2020/Ödev-2/Ödev2_MekanizmaAnimasyonOrnekleri1_41/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ödev2_MekanizmaAnimasyonOrnekleri1_41
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Graphics CizimAlani;
+        Graphics GrafikAlani1;
+        Graphics GrafikAlani2;
+
+        Pen Kalem1 = new Pen(System.Drawing.Color.Yellow, 3);
+        Pen Kalem2 = new Pen(System.Drawing.Color.White, 3);
+        Pen Kalem3 = new Pen(System.Drawing.Color.DarkOrange, 3);
+        Pen Kalem4 = new Pen(System.Drawing.Color.Aqua, 3);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CizimAlani = pictureBox1.CreateGraphics();
+            GrafikAlani1 = pictureBox3.CreateGraphics();
+            GrafikAlani2 = pictureBox4.CreateGraphics();
+
+            trackBar1.Maximum = 20;
+            trackBar1.Minimum = 1;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = e.X.ToString();
+            textBox2.Text = e.Y.ToString();
+
+            CizimAlani.DrawLine(Kalem1, Convert.ToInt32(textBox1.Text) - 5, Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox1.Text) + 5, Convert.ToInt32(textBox2.Text));
+            CizimAlani.DrawLine(Kalem1, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text) - 5, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text) + 5);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CizimAlani.DrawLine(Kalem1, Convert.ToInt32(textBox1.Text) - 5, Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox1.Text) + 5, Convert.ToInt32(textBox2.Text));
+            CizimAlani.DrawLine(Kalem1, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text) - 5, Convert.ToInt32(textBox1.Text), Convert.ToInt32(textBox2.Text) + 5);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+        }
+
+        public void cizim()
+        {
+            CizimAlani.DrawString("Cengizhan Topçu", new Font("Vineta BT", 20), new SolidBrush(Color.Red), 150, 525);
+            CizimAlani.DrawString("2017010225048", new Font("Vineta BT", 20), new SolidBrush(Color.Red), 170, 550);
+
+            CizimAlani.DrawEllipse(Kalem1, 250, 50, 180, 180); //No:1
+            CizimAlani.DrawEllipse(Kalem1, 315, 118, 50, 50); //No:2.1
+            CizimAlani.DrawEllipse(Kalem4, 315, 243, 50, 50); //No:2.2
+            CizimAlani.DrawEllipse(Kalem2, 325, 128, 30, 30); ////No:3.1
+            CizimAlani.DrawEllipse(Kalem2, 325, 253, 30, 30); ////No:3.2
+            CizimAlani.DrawRectangle(Kalem3, 90, 400, 500, 30); //No:4
+            CizimAlani.DrawRectangle(Kalem2, 130, 380, 30, 70); //No:5.1
+            CizimAlani.DrawRectangle(Kalem2, 515, 380, 30, 70); //No:5.2
+            //CizimAlani.DrawEllipse(Kalem1, 350, 200, 20, 20); //No:6
+
+            //PointF point1 = new PointF(240, 400);
+            //PointF point2 = new PointF(247.5F, 380);
+            //PointF point3 = new PointF(253.5F, 380);
+            //PointF point4 = new PointF(261, 400);
+            //PointF[] poligannoktaları = { point1, point2, point3, point4 }; //No:7.1
+            PointF[] poligannoktaları1 = { new PointF(240, 400), new PointF(247.5F, 380), new PointF(253.5F, 380), new PointF(261, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları1); //No:7.1
+            PointF[] poligannoktaları2 = { new PointF(267, 400), new PointF(274.5F, 380), new PointF(280.5F, 380), new PointF(288, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları2); //No:7.2
+            PointF[] poligannoktaları3 = { new PointF(294, 400), new PointF(301.5F, 380), new PointF(307.5F, 380), new PointF(315, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları3); //No:7.3
+            PointF[] poligannoktaları4 = { new PointF(321, 400), new PointF(328.5F, 380), new PointF(334.5F, 380), new PointF(342, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları4); //No:7.4
+            PointF[] poligannoktaları5 = { new PointF(348, 400), new PointF(355.5F, 380), new PointF(361.5F, 380), new PointF(369, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları5); //No:7.5
+            PointF[] poligannoktaları6 = { new PointF(375, 400), new PointF(382.5F, 380), new PointF(388.5F, 380), new PointF(396, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları6); //No:7.6
+            PointF[] poligannoktaları7 = { new PointF(402, 400), new PointF(409.5F, 380), new PointF(415.5F, 380), new PointF(423, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları7); //No:7.7
+            PointF[] poligannoktaları8 = { new PointF(427, 400), new PointF(434.5F, 380), new PointF(440.5F, 380), new PointF(448, 400) };
+            CizimAlani.DrawPolygon(Kalem3, poligannoktaları8); //No:7.8
+        }
+
+        int saniye = 0, salise = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            salise++;
+            label7.Text = salise.ToString();
+            if (salise==60)
+            {
+                saniye++;
+                label5.Text = saniye.ToString();
+                salise = 0;
+                if (saniye==20)
+                {
+                    saniye = 0;
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            //int [] degerler = { 5, 10, 15, 20};
+            label6.Text = trackBar1.Value.ToString();
+            timer1.Interval = Convert.ToInt32(label6.Text);
+        }
+
+        int X1, Y1;
+        double X2, Y2;
+        int Alfa = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+            X1 = 330;
+            Y1 = 130;
+            int L = 74;
+            Alfa++;
+            textBox3.Text = Alfa.ToString();
+            double AlfaRadyan = Alfa * 2 * Math.PI / 360; 
+            X2 = X1 + L * Math.Cos(AlfaRadyan);
+            Y2 = Y1 + L * Math.Sin(AlfaRadyan);
+            CizimAlani.DrawEllipse(Kalem1, Convert.ToInt32(X2), Convert.ToInt32(Y2), 20, 20); //No:6
+            //CizimAlani.DrawLine(Kalem4, X1, Y1, Convert.ToInt32(X2), Convert.ToInt32(Y2)); //No:0
+            cizim();
+
+            //Dönen Diskin Hız-Zaman Grafiği
+            GrafikAlani1.DrawString("Dönen Disk", new Font("Calibri", 14), new SolidBrush(Color.White), 160, 3);
+            GrafikAlani1.DrawLine(Kalem3, 40, 20, 40, 195); //Hız Çizgisi
+            GrafikAlani1.DrawLine(Kalem3, 40, 170, 345, 170); //Zaman Çizgisi
+            GrafikAlani1.DrawString("Hız", new Font("Calibri", 12), new SolidBrush(Color.White), 27, 2);
+            GrafikAlani1.DrawString("5", new Font("Calibri", 12), new SolidBrush(Color.White), 23, 140);
+            GrafikAlani1.DrawString("10", new Font("Calibri", 12), new SolidBrush(Color.White), 15, 100);
+            GrafikAlani1.DrawString("15", new Font("Calibri", 12), new SolidBrush(Color.White), 15, 60);
+            GrafikAlani1.DrawString("20", new Font("Calibri", 12), new SolidBrush(Color.White), 15, 20);
+            GrafikAlani1.DrawString("Zaman", new Font("Calibri", 12), new SolidBrush(Color.White), 346, 160);
+            GrafikAlani1.DrawString("1", new Font("Calibri", 12), new SolidBrush(Color.White), 60, 175);
+            GrafikAlani1.DrawString("2", new Font("Calibri", 12), new SolidBrush(Color.White), 90, 175);
+            GrafikAlani1.DrawString("3", new Font("Calibri", 12), new SolidBrush(Color.White), 120, 175);
+            GrafikAlani1.DrawString("4", new Font("Calibri", 12), new SolidBrush(Color.White), 150, 175);
+            GrafikAlani1.DrawString("5", new Font("Calibri", 12), new SolidBrush(Color.White), 180, 175);
+            GrafikAlani1.DrawString("6", new Font("Calibri", 12), new SolidBrush(Color.White), 210, 175);
+            GrafikAlani1.DrawString("7", new Font("Calibri", 12), new SolidBrush(Color.White), 240, 175);
+            GrafikAlani1.DrawString("8", new Font("Calibri", 12), new SolidBrush(Color.White), 270, 175);
+            GrafikAlani1.DrawString("9", new Font("Calibri", 12), new SolidBrush(Color.White), 300, 175);
+            GrafikAlani1.DrawString("10", new Font("Calibri", 12), new SolidBrush(Color.White), 330, 175);
+
+            //Kramayer Dişlisinin Hız-Zaman Grafiği
+            GrafikAlani2.DrawString("Kramayer Dişlisi", new Font("Calibri", 14), new SolidBrush(Color.White), 145, 3);
+            GrafikAlani2.DrawLine(Kalem3, 40, 20, 40, 195); //Hız Çizgisi
+            GrafikAlani2.DrawLine(Kalem3, 40, 170, 345, 170); //Zaman Çizgisi
+            GrafikAlani2.DrawString("Hız", new Font("Calibri", 12), new SolidBrush(Color.White), 27, 2);
+            GrafikAlani2.DrawString("5", new Font("Calibri", 12), new SolidBrush(Color.White), 23, 140);
+            GrafikAlani2.DrawString("10", new Font("Calibri", 12), new SolidBrush(Color.White), 15, 100);
+            GrafikAlani2.DrawString("15", new Font("Calibri", 12), new SolidBrush(Color.White), 15, 60);
+            GrafikAlani2.DrawString("20", new Font("Calibri", 12), new SolidBrush(Color.White), 15, 20);
+            GrafikAlani2.DrawString("Zaman", new Font("Calibri", 12), new SolidBrush(Color.White), 346, 160);
+            GrafikAlani2.DrawString("1", new Font("Calibri", 12), new SolidBrush(Color.White), 60, 175);
+            GrafikAlani2.DrawString("2", new Font("Calibri", 12), new SolidBrush(Color.White), 90, 175);
+            GrafikAlani2.DrawString("3", new Font("Calibri", 12), new SolidBrush(Color.White), 120, 175);
+            GrafikAlani2.DrawString("4", new Font("Calibri", 12), new SolidBrush(Color.White), 150, 175);
+            GrafikAlani2.DrawString("5", new Font("Calibri", 12), new SolidBrush(Color.White), 180, 175);
+            GrafikAlani2.DrawString("6", new Font("Calibri", 12), new SolidBrush(Color.White), 210, 175);
+            GrafikAlani2.DrawString("7", new Font("Calibri", 12), new SolidBrush(Color.White), 240, 175);
+            GrafikAlani2.DrawString("8", new Font("Calibri", 12), new SolidBrush(Color.White), 270, 175);
+            GrafikAlani2.DrawString("9", new Font("Calibri", 12), new SolidBrush(Color.White), 300, 175);
+            GrafikAlani2.DrawString("10", new Font("Calibri", 12), new SolidBrush(Color.White), 330, 175);
+        }
+    }
+}
